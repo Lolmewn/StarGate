e896e4f5e79ae7d981222b6661238d6d14de3353
Lolmewn <Lolmewn@SybrensPC>
2012-02-23 11:51:10 +0100

    First commit

---
+++ src/main/java/nl/lolmewn/Stargate/Main.java
@@ -0,0 +1,19 @@
+package nl.lolmewn.Stargate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+
+	@Override
+	public void onDisable() {
+		
+	}
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	
+
+}
